82ae06aca31c92f6f5af5ec8ae784156ca5cc3d1
wooknick <wooknick@naver.com>
2020-03-18 02:20:21 +0900

    0.82

---
+++ docs/control copy.js
@@ -0,0 +1,255 @@
+var textInit = () => {
+  document.getElementsByClassName("pages_text")[0].innerHTML = `1 / ${
+    document.getElementsByClassName("content_cell").length
+  }`;
+};
+textInit();
+
+// audio setting
+var soundInit = false;
+var t;
+var d;
+var audio = new (window.AudioContext || window.webkitAudioContext)();
+
+// tone setting 정보
+var wave = "triangle"; // sine, sawtooth, square, triangle, custom
+var key = "g"; // c, d, e, f, g, a, b
+var mod = ""; // "", #, b
+var scale = "major"; // Pentatonic, Major, Minor, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian
+var activity = 4000; // 1000, 2000, 4000, 8000
+var attack = 250; // 10, 250
+var voices = 1; // 1, 2, 3, 4, 5
+var masterVolume = 50; // 1 - 100;
+
+// 모바일 버퍼 생성
+window.addEventListener(
+  "touchstart",
+  function() {
+    // create empty buffer
+    var buffer = audio.createBuffer(1, 1, 22050);
+    var source = audio.createBufferSource();
+    // alert(source);
+    source.buffer = buffer;
+    // connect to output (your speakers)
+    source.connect(audio.destination);
+    // play the file
+    source.noteOn(0);
+  },
+  false
+);
+
+if (window.speechSynthesis) {
+  var announce = window.speechSynthesis;
+}
+
+function setMotion(emotion) {
+  if (emotion === 1) {
+    wave = "sine";
+    key = "c";
+    mod = "#"; //
+    scale = "minor";
+    activity = 4000;
+    attack = 250;
+    voices = 1;
+    masterVolume = 50;
+  } else if (emotion === 2) {
+    wave = "square";
+    key = "a";
+    mod = "M"; //
+    scale = "Lydian";
+    activity = 3000;
+    attack = 250;
+    voices = 2;
+    masterVolume = 70;
+  }
+}
+
+function getTheme() {
+  let notes = Tonal.Scale.notes(key + mod + " " + scale);
+  let returnTheme = new Array();
+  for (var i = 2; i <= 5; i++) {
+    for (var j = 0; j < notes.length; j++) {
+      returnTheme.push(notes[j] + i + "");
+    }
+  }
+  console.log("playing phrase: " + key + mod + " " + scale);
+  // returnTheme : ["G#2", "A#2", "B#2", "D#2", "E#2", "G#3", "A#3", "B#3", "D#3", "E#3", "G#4", "A#4", "B#4", "D#4", "E#4", "G#5", "A#5", "B#5", "D#5", "E#5"]
+  return returnTheme;
+}
+
+function separateTones(theme, octave) {
+  let theseNotes = new Array();
+  theseNotes["drone"] = new Array();
+  theseNotes["melody"] = new Array();
+  for (a = 0; a < theme.length; a++) {
+    if (theme[a].match(/\d+/)[0] <= octave) {
+      theseNotes["drone"].push(theme[a]);
+    } else {
+      theseNotes["melody"].push(theme[a]);
+    }
+  }
+  return theseNotes;
+}
+
+function synth(w, f, v, a, l, d, x, y, z) {
+  v = v * (masterVolume / 100);
+
+  if (v == 0) {
+    return;
+  }
+
+  var gainNode = audio.createGain();
+  var osc = audio.createOscillator();
+  gainNode.connect(audio.destination);
+  gainNode.gain.setValueAtTime(0, audio.currentTime);
+  gainNode.gain.linearRampToValueAtTime(v, audio.currentTime + a / 1000);
+  gainNode.gain.linearRampToValueAtTime(
+    0,
+    audio.currentTime + (a / 1000 + l / 1000 + d / 1000)
+  );
+
+  panner = audio.createPanner();
+  panner.setPosition(x, y, z);
+  osc.connect(panner);
+  panner.connect(audio.destination);
+
+  osc.frequency.value = f;
+  osc.type = w;
+  osc.connect(gainNode);
+  osc.start(0);
+
+  var stoptime = (a + l + d) * 1.1;
+
+  setTimeout(function() {
+    gainNode.gain.linearRampToValueAtTime(0, audio.currentTime + 5);
+    osc.stop(audio.currentTime + 7);
+    osc.disconnect(gainNode);
+    gainNode.disconnect(audio.destination);
+  }, stoptime);
+}
+
+function play() {
+  for (voice = 0; voice < voices; voice++) {
+    var nextPhrase = 0; // 재생 시간 관련인듯
+    let theme = getTheme(); // 스케일에 따라 note 확보
+    let thisPhrase = separateTones(theme, 2); // note 구분 - 베이스 영역 / 멜로디 영역
+    let drone =
+      thisPhrase["drone"][
+        Math.floor(Math.random() * thisPhrase["drone"].length)
+      ];
+    let phraseLen = Math.floor(Math.random() * thisPhrase["melody"].length);
+    let phrase = new Array();
+    for (a = 0; a < phraseLen; a++) {
+      phrase.push(
+        thisPhrase["melody"][
+          Math.floor(Math.random() * thisPhrase["melody"].length)
+        ]
+      );
+    }
+
+    // melody 노트 예약 일괄 등록
+    var noteDelay = 0;
+    for (b = 0; b <= phrase.length; b++) {
+      let volume = Math.random() * 0.15;
+      let delay = Math.random() * 100 + 150;
+      let length = activity * ((Math.floor(Math.random() * 8) + 1) / 8);
+      let release = attack + length;
+      nextPhrase += release;
+      let x = Math.random() * 300 - 150;
+      let y = Math.random() * 300 - 150;
+      let z = Math.random() * 300 - 150;
+      if (b < phrase.length) {
+        t = setTimeout(
+          `synth("${wave}", ${Tonal.Note.freq(
+            phrase[b]
+          )}, ${volume}, ${attack}, ${length}, ${delay}, ${x}, ${y}, ${z})`,
+          noteDelay + Math.random() * 1000
+        );
+      }
+      noteDelay += length;
+    }
+
+    // drone 노트 예약 등록
+    for (c = 0; c <= 1; c++) {
+      let volume = Math.random() * 0.15;
+      let delay = Math.random() * 100 + 150;
+      let x = Math.random() * 300 - 150;
+      let y = Math.random() * 300 - 150;
+      let z = Math.random() * 300 - 150;
+
+      d = setTimeout(
+        `synth("${wave}",
+        ${Tonal.Note.freq(
+          drone
+        )}, ${volume}, ${attack}, ${nextPhrase}, ${delay}, ${x}, ${y}, ${z})
+        `,
+        voice * (Math.random() * 1000)
+      );
+    }
+  }
+
+  t = setTimeout(function() {
+    play();
+  }, nextPhrase);
+}
+
+function stop() {
+  console.log(source);
+  source.stop(0);
+}
+
+// 슬라이드 설정 시작
+var preIndex = -1;
+var elem = document.querySelector(".content");
+var flkty = new Flickity(elem, {
+  // options
+  cellAlign: "left",
+  dragThreshold: 10,
+  pageDots: false,
+  prevNextButtons: false,
+  on: {
+    ready: function() {
+      console.log("start");
+    },
+    change: function(index) {
+      pageUpdate(index);
+      if (!soundInit) {
+        // playSound("init");
+        play();
+        soundInit = true;
+      }
+      if (preIndex === 2 && index === 3) {
+        // playSound("star_gazing");
+        // stop();
+        setMotion(1);
+        // play();
+      } else if (preIndex === 5 && index === 6) {
+        setMotion(2);
+        // playSound("magical_journey");
+      } else if (preIndex === 6 && index === 5) {
+        // playSound("star_gazing");
+        setMotion(1);
+      } else if (preIndex === 3 && index === 2) {
+        setMotion(2);
+        // playSound("dawn");
+      }
+      preIndex = index;
+    }
+  }
+});
+
+flkty.on("staticClick", () => {
+  //   if (!nowPlaySource) {
+  //     playSound("dawn");
+  //   }
+  // if (!soundInit) {
+  //   playSound("init");
+  //   soundInit = true;
+  // }
+});
+
+var pageUpdate = page => {
+  document.getElementsByClassName("pages_text")[0].innerHTML = `${page + 1} / ${
+    document.getElementsByClassName("content_cell").length
+  }`;
+};
